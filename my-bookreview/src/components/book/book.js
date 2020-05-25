e18564fd62bc564c8169d8a566ca8860ddbdf2fc
Gahana-Kundur <gahanakundur@gmail.com>
2020-05-25 16:01:47 -0700

    Completed with books display, implemented book styles. May 25th. Fifth commit

--- my-bookreview/src/components/book/book.js
+++ my-bookreview/src/components/book/book.js
@@ -1,20 +1,44 @@
 import React from 'react'
 import styles from './book.module.scss'
-import MalgudiDays from './malgudidays.jpg'
-import TheAlchemist from './thealchemist.jpg'
+import malgudiPic from './malgudidays.jpg'
+import alchemistPic from './thealchemist.jpg'
 
 function Book(props){
+  let pic;
+  let tags=(
+    <p>
+      {props.tags.map((tag)=>(
+        <span>{tag}</span>
+      ))}
+    </p>
+
+  )
+
+  let metadata;
+  if(props.complete){
+    metadata= <p>{'⭐'.repeat(props.rating)}</p>
+  }else{
+    metadata=<p><progress value={props.progress} max="100" /></p>
+  }
+
+  if(props.title === 'Malgudi days'){
+    pic = malgudiPic
+  } else if (props.title === 'The Alchemist') {
+    pic = alchemistPic
+  }
+
+
   return(
 
     <article className={styles.book}>
       <picture className={styles.picture}>
-        <img src={props.imageUrl} alt={props.title} />
+        <img src={pic} alt={props.title} />
       </picture>
+
       <h1>{props.title}</h1>
       <h2>{props.author}</h2>
-      <p>{'⭐'.repeat(props.rating)}</p>
-      <p>{props.progress}</p>
-      <p>{props.tags}</p>
+      {metadata}
+      {tags}
     </article>
 
 
